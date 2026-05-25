74a5d5d75e245d8cf7d1b213fc01add35456391d
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2026-05-25 17:26:32 +0530

    new scene

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,31 @@
 #include "texture.h"
 // clang-format on
 
-int main() {
+void checkered_spheres() {
+  hittable_list world;
+  shared_ptr<texture> checker = make_shared<checker_texture>(
+      0.32, color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
 
+  world.add(make_shared<sphere>(point3(0, -10, 0.), 10,
+                                make_shared<lambertian>(checker)));
+  world.add(make_shared<sphere>(point3(0, 10, 0.), 10,
+                                make_shared<lambertian>(checker)));
+
+  camera cam;
+  cam.aspect_ratio = 16 / 9.0;
+  cam.image_width = 400;
+  cam.samples_per_pixel = 100;
+  cam.max_depth = 50;
+
+  cam.vFov = 20;
+  cam.lookfrom = point3(13, 2, 3);
+  cam.lookat = vec3(0, 1, 0);
+  cam.vUp = vec3(0, 1, 0);
+
+  cam.defocus_angle = 0;
+  cam.render(world);
+}
+void bouncing_spheres() {
   camera cam;
   cam.image_width = 400;
   cam.aspect_ratio = 16.0 / 9.0;
@@ -92,4 +115,9 @@ int main() {
                                                                      start)
                    .count()
             << "ms\n";
+}
+
+int main() {
+  // bouncing_spheres();
+  checkered_spheres();
 }
