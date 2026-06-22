f343e8997366ec5001ecac48098651949a351b7c
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2026-06-22 18:17:58 +0530

    minor clean up

--- srcuda/main.cu
+++ srcuda/main.cu
@@ -32,63 +32,6 @@ GPUImage load_image_to_gpu(const char* filename) {
     return img;
 }
 
-void cornell_box() {
-  hittable_list world;
-
-  auto red = make_shared<lambertian>(color(.65, .05, .05));
-  auto white = make_shared<lambertian>(color(.73, .73, .73));
-  auto green = make_shared<lambertian>(color(.12, .45, .15));
-  auto light = make_shared<diffuse_light>(color(15, 15, 15));
-
-  world.add(make_shared<quad>(point3(555, 0, 0), vec3(0, 555, 0),
-                              vec3(0, 0, 555), green));
-  world.add(make_shared<quad>(point3(0, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555),
-                              red));
-  world.add(make_shared<quad>(point3(343, 554, 332), vec3(-130, 0, 0),
-                              vec3(0, 0, -105), light));
-  world.add(make_shared<quad>(point3(0, 0, 0), vec3(555, 0, 0), vec3(0, 0, 555),
-                              white));
-  world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0),
-                              vec3(0, 0, -555), white));
-  world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0),
-                              vec3(0, 555, 0), white));
-
-  shared_ptr<hittable> box1 =
-      box(point3(0, 0, 0), point3(165, 330, 165), white);
-  box1 = make_shared<rotate_y>(box1, 15);
-  box1 = make_shared<translate>(box1, vec3(265, 0, 295));
-  world.add(box1);
-
-  shared_ptr<hittable> box2 =
-      box(point3(0, 0, 0), point3(165, 165, 165), white);
-  box2 = make_shared<rotate_y>(box2, -18);
-  box2 = make_shared<translate>(box2, vec3(130, 0, 65));
-  world.add(box2);
-  camera cam;
-
-  cam.aspect_ratio = 1.0;
-  cam.image_width = 600;
-  cam.samples_per_pixel = 200;
-  cam.max_depth = 50;
-  cam.background = color(0, 0, 0);
-  cam.use_sky_gradient = false;
-
-  cam.vFov = 40;
-  cam.lookfrom = point3(278, 278, -800);
-  cam.lookat = point3(278, 278, 0);
-  cam.vUp = vec3(0, 1, 0);
-
-  cam.defocus_angle = 0;
-
-  std::chrono::time_point start = std::chrono::high_resolution_clock::now();
-  cam.render(world);
-  std::chrono::time_point end = std::chrono::high_resolution_clock::now();
-  std::clog << "Total: "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(end -
-                                                                     start)
-                   .count()
-            << "ms\n";
-}
 __global__ void create_cornell_box(hittable_list* world, camera* cam, GPUImage* textures, curandState* state) {
     if(!(threadIdx.x == 0 && blockIdx.x == 0)) return;
     //init_world
@@ -194,7 +137,7 @@ int main() {
   camera* d_cam;
   //create the scene use the init state rand
   switch (scene_number) {
-  case 1: 
+  case 1: //cornell box
     image_width = 600;
     aspect_ratio=1.0f;
     image_height = int(image_width / aspect_ratio);
